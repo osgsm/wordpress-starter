bb6c7e074e2f9bd07da1273391badc043302379c
osgsm <50955473+osgsm@users.noreply.github.com>
2024-01-08 23:12:53 +0900

    Update asset

--- wp-content/plugins/my-plugin/blocks/wp/build/index.asset.php
+++ wp-content/plugins/my-plugin/blocks/wp/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '199ce409030539b62e3e');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '5805bf4fb916a40cac40');
